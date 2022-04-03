16fb116a05adbc743147571978631c320c6cff59
Frank Udoags <frankudoags@gmail.com>
2022-04-03 22:35:22 +0100

    Initial Commit

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,9 +5,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
-
   //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
+  uint256 public constant tokensPerEth = 100;
+event BuyTokens(address buyer, uint256 amountOfETH,uint256 amountOfTokens);
+ event SellTokens(address seller,uint256 amountOfTokens, uint256 amountOfETH);
   YourToken public yourToken;
 
   constructor(address tokenAddress) {
@@ -15,9 +16,29 @@ contract Vendor is Ownable {
   }
 
   // ToDo: create a payable buyTokens() function:
-
+  function buyTokens() public payable {
+    require(
+            yourToken.balanceOf(address(this)) >= (msg.value * tokensPerEth),
+            "ERC20: transfer amount exceeds balance"
+        );
+         uint256 amount = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, amount);
+        emit BuyTokens(msg.sender, msg.value, amount);
+  }
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public payable onlyOwner {
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
 
   // ToDo: create a sellTokens() function:
-
+    function sellTokens(uint256 amount) public payable {
+      require(yourToken.balanceOf(msg.sender) >= amount,"ERC20: Sell amount exceeds balance");
+        yourToken.approve(address(this), amount);
+        yourToken.transferFrom(msg.sender, address(this), amount);
+        uint256 ethAmount = amount / tokensPerEth;
+        (bool sent, ) = msg.sender.call{value: ethAmount}("");
+        require(sent, "Failed to send Ether");
+        emit SellTokens(msg.sender, amount, ethAmount);
+    }
 }
